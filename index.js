e6f0a69178561a61512ddfb11b0da03aec2bac53
GunnuD <38685931+GunnuD@users.noreply.github.com>
2021-08-21 17:35:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-var http = require('http');
- http.createServer(function (req, res) {
-   res.writeHead(200, {'Content-Type': 'text/html'});
-   res.end('Welcome to DSPA :)!');
- }).listen(3030);
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My Welcome To DSPA family</h2>
+<p>My Happy Raksha Bandan.</p>
+
+<p>Never call document.write after the document has finished loading.
+It will overwrite the whole document.</p>
+
+<script>
+document.write(5 + 6);
+</script>
+
+</body>
+</html> 
