246b1745a44c7dec8715ede541f4f9a88497abc2
GunnuD <38685931+GunnuD@users.noreply.github.com>
2021-09-17 13:28:11 +0530

    update

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,12 @@
 tar czf easyio.tar.gz main.js package.json public LICENSE
 scp easyio.tar.gz 172.31.7.230:~
 rm easyio.tar.gz
-
 ssh 172.31.7.230 << 'ENDSSH'
 pm2 stop all
-rm -rf easyio
 mkdir easyio
 tar xf easyio.tar.gz -C easyio
 rm easyio.tar.gz
 cd easyio 
-npm i
+npm install
 pm2 start all
 ENDSSH
